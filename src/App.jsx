2262693b785cd51726908d0563c58c06b7b350f7
Natalia-Havashi <natali.gavashi@gmail.com>
2025-05-02 23:14:32 +0300

    feat: finish calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import InvestmentForm from "./components/InvestmentForm";
+import List from "./components/List";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return { ...prevUserInput, [inputIdentifier]: +newValue };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <InvestmentForm userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <List userInput={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,42 @@
+import { calculateInvestmentResults } from "../util/investment.js";
+import { formatter } from "../util/investment.js";
+
+export default function List({ userInput }) {
+  const resultData = calculateInvestmentResults(userInput);
+  const initialInvestment =
+    resultData[0].valueEndOfYear -
+    resultData[0].interest -
+    resultData[0].annualInvestment;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map(
+          ({ year, interest, valueEndOfYear, annualInvestment }) => {
+            const totalInterest =
+              valueEndOfYear - annualInvestment * year - initialInvestment;
+            const totalAmountInvested = (valueEndOfYear = totalInterest);
+            return (
+              <tr key={year}>
+                <td>{year}</td>
+                <td>{formatter.format(valueEndOfYear)}</td>
+                <td>{formatter.format(interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+              </tr>
+            );
+          }
+        )}
+      </tbody>
+    </table>
+  );
+}
